252f5ea33be776f5a2a66b9921c3304044221393
lyae <lyaesley@gmail.com>
2023-03-16 00:02:10 +0900

    Initial commit

---
+++ src/test/java/com/lyae/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lyae.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class HelloSpringApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
