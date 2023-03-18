4405207053b8d6515a451384ed22b0c4484fa41a
lyae <lyaesley@gmail.com>
2023-03-19 00:20:21 +0900

    AOP 완료 / 강의 전체 완료!

---
+++ src/main/java/com/lyae/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package com.lyae.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+//@Component
+public class TimeTraceAop {
+
+    @Around("execution(* com.lyae.hellospring..*(..)) && !target(com.lyae.hellospring.SpringConfig)")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toLongString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toLongString() + " " + timeMs + "ms");
+        }
+    }
+}
